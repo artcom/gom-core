aca28cba533ffa9c9d3949acabdf64d6b11f4c84
dirk lüsebrink <dirk.luesebrink@gmail.com>
2013-08-19 12:10:33 +0200

    fixed: Tempfile usage error in logger spec
    and minor Gemfile and Guardfile updates

--- Gemfile
+++ Gemfile
@@ -1,3 +1,3 @@
-source :rubygems
+source 'https://rubygems.org'
 
 gemspec # see gom-core.gemspec
--- spec/gom/logger_spec.rb
+++ spec/gom/logger_spec.rb
@@ -1,4 +1,4 @@
-require File.dirname(__FILE__)+'/../spec_helper'
+require 'spec_helper'
 
 describe Gom::Logger do
   it "should find the class" do
@@ -6,23 +6,20 @@ describe Gom::Logger do
   end
 
   describe "with a Logger" do
-    before :each do
-      require 'tempfile'
-      @logfile = Tempfile.new(__FILE__)
-      @log = Gom::Logger.new @logfile.path
-    end
+    let(:logfile) { Tempfile.new(File.basename(__FILE__)) }
+    let(:log)     { Gom::Logger.new logfile.path }
 
     it "should have a default 'I' log level" do
-      @log.info self.to_s
-      (line = @logfile.read).should_not == nil
+      log.info self.to_s
+      (line = logfile.read).should_not == nil
       line.should =~ / I #{self.to_s}/
     end
 
     it "should log execeptions" do
-      raise "foo" rescue @log.ex $!, "bar"
-      (txt = @logfile.read).should_not == nil
+      raise "foo" rescue log.ex $!, "bar"
+      (txt = logfile.read).should_not == nil
 
-      #assert_match " E RuntimeError: bar", txt, @logfile.path
+      #assert_match " E RuntimeError: bar", txt, logfile.path
       txt.should =~ / E foo: bar/
       txt.should =~ / D foo -- callstack: bar/
     end
