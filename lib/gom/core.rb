b0a5d980686c600009b389ba058798c25ca1614b
dirk lüsebrink <dirk.luesebrink@gmail.com>
2013-08-20 14:48:13 +0200

    create module namespace in top-level include file

--- lib/gom/core.rb
+++ lib/gom/core.rb
@@ -1,3 +1,5 @@
+module Gom; module Core; end; end
+
 require 'gom/logger'
 require 'gom/core/primitive'
 require 'gom/core/version'
--- lib/gom/core/primitive.rb
+++ lib/gom/core/primitive.rb
@@ -1,8 +1,3 @@
-module Gom
-  module Core
-  end
-end
-
 class Gom::Core::Primitive
 
   Types = {
